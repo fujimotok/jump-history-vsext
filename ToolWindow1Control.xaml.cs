7281bfa020c1483f6542fbda26a191e1aaf871a4
fujimotok <fujimotok@users.noreply.github.com>
2025-01-13 19:33:57 +0900

    ジャンプ機能を実装

--- ToolWindow1Control.xaml.cs
+++ ToolWindow1Control.xaml.cs
@@ -17,18 +17,17 @@ namespace jump_history
             this.InitializeComponent();
         }
 
-        /// <summary>
-        /// Handles click on the button by displaying a message box.
-        /// </summary>
-        /// <param name="sender">The event sender.</param>
-        /// <param name="e">The event args.</param>
-        [SuppressMessage("Microsoft.Globalization", "CA1300:SpecifyMessageBoxOptions", Justification = "Sample code")]
-        [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:ElementMustBeginWithUpperCaseLetter", Justification = "Default event handler naming pattern")]
-        private void button1_Click(object sender, RoutedEventArgs e)
+        private void list_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
-            MessageBox.Show(
-                string.Format(System.Globalization.CultureInfo.CurrentUICulture, "Invoked '{0}'", this.ToString()),
-                "ToolWindow1");
+            var vm = this.DataContext as ToolWindow1VM;
+            if (vm != null)
+            {
+                var item = (sender as ListView).SelectedItem as LineInfo;
+                if (item != null)
+                {
+                    vm.JumpTo(item);
+                }
+            }
         }
     }
 }
--- ToolWindow1VM.cs
+++ ToolWindow1VM.cs
@@ -1,4 +1,5 @@
 ﻿using EnvDTE;
+using System;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
@@ -9,13 +10,21 @@ namespace jump_history
     {
         public event PropertyChangedEventHandler PropertyChanged;
         private void RaisePropertyChanged([CallerMemberName] string propertyName = null) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        
+
+        private EnvDTE.DTE _dte;
+
         public ToolWindow1VM(DTE dte)
         {
             this._dte = dte;
         }
 
-        private EnvDTE.DTE _dte;
+        public void JumpTo(LineInfo item)
+        {
+            Microsoft.VisualStudio.Shell.ThreadHelper.ThrowIfNotOnUIThread();
+            this._dte.ItemOperations.OpenFile(item.FileName);
+            var selection = (TextSelection)this._dte.ActiveDocument.Selection;
+            selection.MoveToLineAndOffset(item.LineNumber, item.ColumnNumber);
+        }
 
         private LineInfo _selectedItem = null;
         public LineInfo SelectedItem
