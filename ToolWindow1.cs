e98c39d39a6b49d8e17384958b0d65ae7a0565d7
fujimotok <fujimotok@users.noreply.github.com>
2025-01-13 10:36:01 +0900

    Windowを追加

---
+++ ToolWindow1.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.Shell;
+using System;
+using System.Runtime.InteropServices;
+
+namespace jump_history
+{
+    /// <summary>
+    /// This class implements the tool window exposed by this package and hosts a user control.
+    /// </summary>
+    /// <remarks>
+    /// In Visual Studio tool windows are composed of a frame (implemented by the shell) and a pane,
+    /// usually implemented by the package implementer.
+    /// <para>
+    /// This class derives from the ToolWindowPane class provided from the MPF in order to use its
+    /// implementation of the IVsUIElementPane interface.
+    /// </para>
+    /// </remarks>
+    [Guid("5b4be84a-b890-406f-9a09-684f7abb7831")]
+    public class ToolWindow1 : ToolWindowPane
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ToolWindow1"/> class.
+        /// </summary>
+        public ToolWindow1() : base(null)
+        {
+            this.Caption = "Jump History";
+
+            // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
+            // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
+            // the object returned by the Content property.
+            this.Content = new ToolWindow1Control();
+        }
+    }
+}
--- jump_historyPackage.cs
+++ jump_historyPackage.cs
@@ -25,6 +25,8 @@ namespace jump_history
     /// </remarks>
     [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [Guid(jump_historyPackage.PackageGuidString)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    [ProvideToolWindow(typeof(ToolWindow1))]
     public sealed class jump_historyPackage : AsyncPackage
     {
         /// <summary>
@@ -46,6 +48,7 @@ namespace jump_history
             // When initialized asynchronously, the current thread may be a background thread at this point.
             // Do any initialization that requires the UI thread after switching to the UI thread.
             await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+            await ToolWindow1Command.InitializeAsync(this);
         }
 
         #endregion
